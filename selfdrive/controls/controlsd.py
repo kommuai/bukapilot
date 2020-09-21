b81ea161c07728d4d873ba8f0803984cc1426472
Comma Device <device@comma.ai>
2020-09-21 10:10:47 +0000

    okay controller

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -345,7 +345,7 @@ class Controls:
 
     # Check if actuators are enabled
     #self.active = self.state == State.enabled or self.state == State.softDisabling
-    self.active = self.state == State.enabled or self.state == State.softDisabling or True
+    self.active = self.state == State.enabled
 
     if self.active:
       self.current_alert_types.append(ET.WARNING)
@@ -407,8 +407,7 @@ class Controls:
     """Send actuators and hud commands to the car, send controlsstate and MPC logging"""
 
     CC = car.CarControl.new_message()
-    #CC.enabled = self.enabled
-    CC.enabled = True
+    CC.enabled = self.enabled
     CC.actuators = actuators
 
     CC.cruiseControl.override = True
