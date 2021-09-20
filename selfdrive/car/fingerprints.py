7b6a7018bbbc7a393dff027f195df4c11cf36249
Comma Device <device@comma.ai>
2021-09-20 14:21:39 +0000

    use shortest fingerprint match method, tweaked perodua bezza interface

--- selfdrive/car/fingerprints.py
+++ selfdrive/car/fingerprints.py
@@ -71,10 +71,11 @@ def eliminate_incompatible_cars(msg, candidate_cars):
         compatible_cars.append(car_name)
         break
 
-  return compatible_cars
-
-  # return the first shortest match. TODO: remove this code 
-  #return [compatible_cars[0]]
+  # to return shortest match
+  if(len(compatible_cars) == 1 or len(compatible_cars) == 0):
+    return compatible_cars
+  else:
+    return [compatible_cars[0]]
 
 
 def all_known_cars():
