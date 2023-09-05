f98d8b5301a0a913ff80e963c3e832010b16d553
iXcess <brandonting@hotmail.com>
2023-09-05 15:25:09 +0800

    Add Jun Jin's toyota TSS2 long tunes

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -49,9 +49,9 @@ def set_long_tune(tune, name):
     tune.deadzoneBP = [0., 8.05]
     tune.deadzoneV = [.0, .14]
     tune.kpBP = [0., 5., 20.]
-    tune.kpV = [1.3, 1.0, 0.7]
+    tune.kpV = [3.9, 3.5, 0.6]
     tune.kiBP = [0., 5., 12., 20., 27.]
-    tune.kiV = [.35, .23, .20, .17, .1]
+    tune.kiV = [.40, .29, .20, .17, .1]
   # Default longitudinal tune
   elif name == LongTunes.TSS:
     tune.deadzoneBP = [0., 9.]
