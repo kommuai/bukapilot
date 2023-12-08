9245c583ce24331ada174bd10cd90f6a65f89e37
iXcess <brandonting@hotmail.com>
2023-12-08 07:18:16 +0000

    Add QC mock car for QC testing
    This vehicle is to simulate an actual vehicle and does not show up in
    the app database as a real drive

--- selfdrive/car/perodua/interface.py
+++ selfdrive/car/perodua/interface.py
@@ -185,6 +185,25 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalActuatorDelayUpperBound = 0.60
       ret.speedControlled = True
 
+    elif candidate == CAR.QC:
+      ret.wheelbase = 2.620
+      ret.steerRatio = 17.00
+      ret.centerToFront = ret.wheelbase * 0.44
+      tire_stiffness_factor = 0.9871
+      ret.mass = 1035. + STD_CARGO_KG
+      ret.wheelSpeedFactor = 1.0
+
+      ret.lateralTuning.pid.kiV, ret.lateralTuning.pid.kpV = [[0.16], [0.30]]
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [255]]
+      ret.lateralTuning.pid.kf = 0.00018
+
+      ret.longitudinalTuning.kpBP = [0., 5., 20.]
+      ret.longitudinalTuning.kpV = [0.65, 0.6, 0.6]
+      ret.longitudinalTuning.kiBP = [5, 7, 28]
+      ret.longitudinalTuning.kiV = [0.12, 0.12, 0.12]
+
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [255]]
+
     else:
       ret.dashcamOnly = True
       ret.safetyModel = car.CarParams.SafetyModel.noOutput
