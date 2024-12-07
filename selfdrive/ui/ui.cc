0fe8fd4fdc0f4489bd8b07eda3dcc438b8a8aed1
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2024-12-07 21:17:38 +0800

    Change minimum brightness from 10% to 3%

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -290,8 +290,8 @@ void Device::updateBrightness(const UIState &s) {
       clipped_brightness = std::pow((clipped_brightness + 16.0) / 116.0, 3.0);
     }
 
-    // Scale back to 10% to 100%
-    clipped_brightness = std::clamp(100.0f * clipped_brightness, 10.0f, 100.0f);
+    // Scale back to 3% to 100%
+    clipped_brightness = std::clamp(100.0f * clipped_brightness, 3.0f, 100.0f);
   }
 
   int brightness = brightness_filter.update(clipped_brightness);
