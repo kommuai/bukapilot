248ceba1714b1c305f918d9730cb1cbfcd564ddf
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2025-07-17 12:39:59 +0800

    Dependencies install scripts from openpilot 0.10.0
    - Add msgpack to dependencies
    - Replace hexdump with function following latest op
    - libQMapLibre.so for PC

--- tools/install_ubuntu_dependencies.sh
+++ tools/install_ubuntu_dependencies.sh
@@ -12,24 +12,20 @@ if [[ ! $(id -u) -eq 0 ]]; then
   SUDO="sudo"
 fi
 
-# Install packages present in all supported versions of Ubuntu
+# Check if stdin is open
+if [ -t 0 ]; then
+  INTERACTIVE=1
+fi
+
+# Install common packages
 function install_ubuntu_common_requirements() {
   $SUDO apt-get update
   $SUDO apt-get install -y --no-install-recommends \
-    autoconf \
-    build-essential \
     ca-certificates \
-    casync \
     clang \
-    cmake \
-    make \
-    cppcheck \
-    libtool \
+    build-essential \
     gcc-arm-none-eabi \
-    bzip2 \
     liblzma-dev \
-    libarchive-dev \
-    libbz2-dev \
     capnproto \
     libcapnp-dev \
     curl \
@@ -42,45 +38,36 @@ function install_ubuntu_common_requirements() {
     libavdevice-dev \
     libavutil-dev \
     libavfilter-dev \
+    libbz2-dev \
     libeigen3-dev \
     libffi-dev \
     libglew-dev \
     libgles2-mesa-dev \
     libglfw3-dev \
     libglib2.0-0 \
+    libjpeg-dev \
+    libqt5charts5-dev \
     libncurses5-dev \
-    libncursesw5-dev \
-    libomp-dev \
-    libopencv-dev \
-    libpng16-16 \
-    libportaudio2 \
     libssl-dev \
-    libsqlite3-dev \
     libusb-1.0-0-dev \
     libzmq3-dev \
+    libzstd-dev \
+    libsqlite3-dev \
     libsystemd-dev \
     locales \
     opencl-headers \
     ocl-icd-libopencl1 \
     ocl-icd-opencl-dev \
-    clinfo \
     portaudio19-dev \
-    qml-module-qtquick2 \
-    qtmultimedia5-dev \
-    qtlocation5-dev \
-    qtpositioning5-dev \
     qttools5-dev-tools \
-    libqt5sql5-sqlite \
     libqt5svg5-dev \
-    libqt5charts5-dev \
     libqt5serialbus5-dev  \
     libqt5x11extras5-dev \
-    libreadline-dev \
-    libdw1 \
-    valgrind
+    libqt5opengl5-dev \
+    xvfb
 }
 
-# Install Ubuntu 22.04 LTS packages
+# Install Ubuntu 24.04 LTS packages
 function install_ubuntu_lts_latest_requirements() {
   install_ubuntu_common_requirements
 
@@ -90,46 +77,50 @@ function install_ubuntu_lts_latest_requirements() {
     qtchooser \
     qt5-qmake \
     qtbase5-dev-tools \
-    python3-dev
-}
-
-# Install Ubuntu 20.04 packages
-function install_ubuntu_focal_requirements() {
-  install_ubuntu_common_requirements
-
-  $SUDO apt-get install -y --no-install-recommends \
-    libavresample-dev \
-    qt5-default \
-    python-dev
+    python3-dev \
+    python3-venv
 }
 
 # Detect OS using /etc/os-release file
 if [ -f "/etc/os-release" ]; then
   source /etc/os-release
   case "$VERSION_CODENAME" in
-    "jammy")
+    "jammy" | "kinetic" | "noble")
       install_ubuntu_lts_latest_requirements
-      ;;
-    "kinetic")
-      install_ubuntu_lts_latest_requirements
-      ;;
-    "focal")
-      install_ubuntu_focal_requirements
       ;;
     *)
-      echo "$ID $VERSION_ID is unsupported. This setup script is written for Ubuntu 20.04."
+      echo "$ID $VERSION_ID is unsupported. This setup script is written for Ubuntu 24.04."
       read -p "Would you like to attempt installation anyway? " -n 1 -r
       echo ""
       if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         exit 1
       fi
-      if [ "$UBUNTU_CODENAME" = "jammy" ] || [ "$UBUNTU_CODENAME" = "kinetic" ]; then
-        install_ubuntu_lts_latest_requirements
-      else
-        install_ubuntu_focal_requirements
-      fi
+      install_ubuntu_lts_latest_requirements
   esac
+
+  if [[ -d "/etc/udev/rules.d/" ]]; then
+    # Setup jungle udev rules
+    $SUDO tee /etc/udev/rules.d/12-panda_jungle.rules > /dev/null <<EOF
+SUBSYSTEM=="usb", ATTRS{idVendor}=="3801", ATTRS{idProduct}=="ddcf", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="3801", ATTRS{idProduct}=="ddef", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="bbaa", ATTRS{idProduct}=="ddcf", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="bbaa", ATTRS{idProduct}=="ddef", MODE="0666"
+
+EOF
+
+    # Setup panda udev rules
+    $SUDO tee /etc/udev/rules.d/11-panda.rules > /dev/null <<EOF
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="3801", ATTRS{idProduct}=="ddcc", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="3801", ATTRS{idProduct}=="ddee", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="bbaa", ATTRS{idProduct}=="ddcc", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="bbaa", ATTRS{idProduct}=="ddee", MODE="0666"
+EOF
+
+    $SUDO udevadm control --reload-rules && $SUDO udevadm trigger || true
+  fi
+
 else
-  echo "No /etc/os-release in the system"
+  echo "No /etc/os-release in the system. Make sure you're running on Ubuntu, or similar."
   exit 1
 fi
