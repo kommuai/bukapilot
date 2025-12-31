3d5fac127bebfd6f492ae34330e4bd3a66e9ed3a
Kommu Device <device@kommu.ai>
2025-12-31 04:09:26 +0000

    controlsd brake disengage logic follow stock

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -325,10 +325,10 @@ class Controls:
     if not self.CP.pcmCruise and not self.v_cruise_helper.v_cruise_initialized and resume_pressed:
       self.events.add(EventName.resumeBlocked)
 
-    # Disable on rising edge of accelerator or brake. Also disable on brake when speed > 0
+    # Disable on rising edge of accelerator. Also disable on brake when speed > 0
     if (CS.gasPressed and not self.CS_prev.gasPressed and self.disengage_on_accelerator) or \
-      (CS.brakePressed and (not self.CS_prev.brakePressed or not CS.standstill)) or \
-      (CS.regenBraking and (not self.CS_prev.regenBraking or not CS.standstill)):
+      (CS.brakePressed and not CS.standstill) or \
+      (CS.regenBraking and not CS.standstill):
       self.events.add(EventName.pedalPressed)
 
     if CS.brakePressed and CS.standstill:
